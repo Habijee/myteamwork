c7ffb0cc6eb313ed27e649973567348241af43fd
steven <lihuayoujiajia@gmail.com>
2021-08-18 20:55:16 -0400

    Added steven_1 and plan

---
+++ plan.py
@@ -0,0 +1,18 @@
+"""
+1. Steven 2021-08-18 Create GitHub repository
+2. Steven 2021-08-18 Send invitation to Teammates / Waiting for members to accept invitation
+3. Kevin  2021-08-18 Successfully receive and join repository
+4.
+5.
+6.
+7.
+8.
+
+
+Finish Day 1 work and commit to GitHub
+
+
+
+
+
+"""
