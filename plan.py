e49967a0436b3d37df64857624f12689cddd6c45
steven <lihuayoujiajia@gmail.com>
2021-08-20 09:44:33 -0400

    updated plan

--- plan.py
+++ plan.py
@@ -1,12 +1,13 @@
 """
 1. Steven 2021-08-18 Create GitHub repository
 2. Steven 2021-08-18 Send invitation to Teammates / Waiting for members to accept invitation
-3. Kevin  2021-08-18 Successfully receive and join repository
-4.
-5.
+3. Steven 2021-08-18 Finish Day 1 work and commit to GitHub
+4. Chengjun 2021-08-20 Successfully receive and join repository
+5. Chengjun 2021-08-20 Finish Day 1 work and commit to GitHub
 6.
 7.
 8.
+9.
 
 
 Finish Day 1 work and commit to GitHub
