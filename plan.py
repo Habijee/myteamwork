2217bcc7cbe64a175f0138bfce050deb497f0891
steven <lihuayoujiajia@gmail.com>
2021-08-20 10:32:58 -0400

    updated plan

--- plan.py
+++ plan.py
@@ -4,7 +4,10 @@
 3. Steven 2021-08-18 Finish Day 1 work and commit to GitHub
 4. Chengjun 2021-08-20 Successfully receive and join repository
 5. Chengjun 2021-08-20 Finish Day 1 work and commit to GitHub
-6.
-7.
+6. Kevin 2021-08-20 Successfully receive and join repository
+7. Kevin 2021-08-20 Finish Day 1 work and commit to GitHub
+8. All members 2021-08-20 Day 1 work completed
+9. Kevin 2021-08-20 Finish Day 2 work and commit to GitHub
+10.
 
 """
